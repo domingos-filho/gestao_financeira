b1a4299685916bda49f693288e1ee27a35e03978
domingos-filho <francisco_adf@hotmail.com>
2026-01-04 18:22:41 -0300

    correcao selecao de perfil de user

--- apps/web/src/components/ui/select.tsx
+++ apps/web/src/components/ui/select.tsx
@@ -19,6 +19,7 @@ const SelectTrigger = React.forwardRef<
       "flex h-10 w-full items-center justify-between rounded-lg border border-border bg-card px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-primary/30",
       className
     )}
+    type="button"
     {...props}
   >
     {children}
